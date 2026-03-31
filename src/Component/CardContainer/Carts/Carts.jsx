0c4ad695375f42f8e36d1b3890faccf70f78a49a
sanjana-moon <sanjanamoonliya@gmail.com>
2026-03-31 23:27:09 +0600

    Proceed button done

--- src/Component/CardContainer/Carts/Carts.jsx
+++ src/Component/CardContainer/Carts/Carts.jsx
@@ -9,6 +9,9 @@ const Carts = ({ selectedCards, setSelectedCards, cardPrice, setCardPrice }) => 
         setSelectedCards(filteredCard)
         setCardPrice(cardPrice - card.price)
     }
+    const handleProceedBtn = () => {
+        setSelectedCards([])
+    }
 
     return (
         <div className='container mx-auto p-4 space-y-6'>
@@ -28,12 +31,15 @@ const Carts = ({ selectedCards, setSelectedCards, cardPrice, setCardPrice }) => 
                                 handleRemoveBtn={handleRemoveBtn} />
                         )
                     })}
-                    <div className='flex justify-between items-center'>
+                    <div className='space-y-5'>
+                        <div className='flex justify-between items-center'>
                         <p className='text-[#627382]'>Total:</p>
                         <p className='text-2xl font-bold'>${cardPrice}</p>
                     </div>
                     <button
+                    onClick={handleProceedBtn}
                         className='btn btn-primary bg-linear-100 from-[#4F39F6] to-[#9514FA] rounded-full w-full'>Proceed to Checkout</button>
+                    </div>
                 </div>
 
             }
